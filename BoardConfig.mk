8aa635fdecd6101a2376ae6cdc1392406581a006
yogevm15 <45556766+yogevm15@users.noreply.github.com>
2021-08-06 01:34:00 +0300

    venus: Moved to common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,23 +11,6 @@ DEVICE_PATH := device/xiaomi/venus
 
 BUILD_BROKEN_DUP_RULES := true
 
-# A/B
-BOARD_USES_RECOVERY_AS_BOOT := true
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    odm \
-    product \
-    system \
-    system_ext \
-    vbmeta \
-    vbmeta_system \
-    vendor \
-    vendor_boot
-
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
