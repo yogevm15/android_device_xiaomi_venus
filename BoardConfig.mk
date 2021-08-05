a4292bdffbc19283ef00c3b59e49d0f2d61c0c57
yogevm15 <45556766+yogevm15@users.noreply.github.com>
2021-08-06 02:05:15 +0300

    venus: Fixing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,8 +9,6 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/venus
 
-BUILD_BROKEN_DUP_RULES := true
-
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
